a31f1b9ea14b3e394be5621bf97a606296958f72
Andrew <ayobamioduola13@gmail.com>
2024-08-20 08:31:11 +0100

    comment functionality updated

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,10 @@ from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail, EmailMessage
+from django.views.decorators.http import require_POST
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
 def post_list(request):
@@ -36,15 +37,21 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)    
-    return render(request, 'blog\post\detail.html', {'post': post})
+    
+    comments =  post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 
+                  'blog\post\detail.html', 
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
 
-
+# using class based view
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = "posts"
     paginate_by = 3
     template_name = 'blog/post/list.html'
-
 
 def post_share(request, post_id):
     # retrive post by id
@@ -73,3 +80,18 @@ def post_share(request, post_id):
 
     return render (request, 'blog/post/share.html', {'form': form,
                                                      'post': post})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blog/post/comment.html', 
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
