fac7fc5f3ad7f1a7b8efd2f33ee4ec0197db709e
Andrew <ayobamioduola13@gmail.com>
2024-08-21 10:04:36 +0100

    tags functionality

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 # Create your models here.
 
 class PublishedManager(models.Manager):
@@ -36,6 +37,7 @@ class Post(models.Model):
 
     objects = models.Manager() # default manager
     published = PublishedManager() # Custom Manager
+    tags = TaggableManager()
 
 
     class Meta:
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,13 +4,20 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail, EmailMessage
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
+from taggit.models import Tag
+
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -20,7 +27,10 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog\post\list.html', {'posts': posts})
+    return render(request, 
+                  'blog\post\list.html', 
+                  {'posts': posts,
+                   'tags': tag})
 
 def post_detail(request, year, month, day, post):
     """try:
